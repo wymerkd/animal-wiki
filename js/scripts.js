7ce7ff8a08388ba2282729470b9dd540cb9f6ada
Student <example@example.com>
2019-10-24 10:44:50 -0700

    finish js functionality

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $("form#animal-form").submit(function(event) {
+
+    var animal = $("input:radio[name=animal]:checked").val();
+
+    $('.hide').hide();
+
+    if (animal === "snake") {
+      $('#snake').show()
+    }
+
+    if (animal === "bird") {
+      $('#bird').show()
+    }
+
+    if (animal === "monkey") {
+      $('#monkey').show()
+    }
+
+    if (animal === "whale") {
+      $('#whale').show()
+    }
+
+    event.preventDefault();
+
+  });
+});
